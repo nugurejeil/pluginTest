01d0c8018d34112976cedf6b9b295cfd0751a345
nugurejeil <nugurejeil@ucsit.co.kr>
2022-01-24 14:31:11 +0900

    add config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path'); // node.js path 모듈을 불러옵니다. 운영체제별로 상이한 경로 문법(구분자 : / 혹은 \)를 해결해 절대 경로로 반환하는 역할을 합니다.
 const webpack = require('webpack'); // 웹팩 모듈을 불러옵니다.
+const childProcess = require('child_process'); // child_process 프로세스 모듈 : 터미널 명령어를 입력할 수 있게 합니다.
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // module.exports : node의 모듈 문법입니다.
@@ -44,7 +45,13 @@ module.exports = {
     plugins: [
         new webpack.BannerPlugin({
             // banner: '배너입니다!!'
-            banner: `last build : ${new Date().toLocaleString()}` // toLocaleString() : 사용자의 환경에 알맞는 형태로 스트링을 반환합니다. 인자로 장소를 지정하지 않으면 브라우저를 참고해 사용자의 장소를 설정합니다.
+            banner:
+                `
+            last build : ${new Date().toLocaleString()}
+            git commit : ${childProcess.execSync('git rev-parse --short HEAD')}
+            committer : ${childProcess.execSync('git config user.name')}
+            `
+            // toLocaleString() : 사용자의 환경에 알맞는 형태로 스트링을 반환합니다. 인자로 장소를 지정하지 않으면 브라우저를 참고해 사용자의 장소를 설정합니다.
         }),
         new HtmlWebpackPlugin({
             template: './src/index.html', // 목표 html파일의 위치입니다.
